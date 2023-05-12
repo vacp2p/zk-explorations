be4a49eaa4dda04ce2a2d19e14a7e913a950074c
tyshkor <tyshko1@gmail.com>
2023-05-12 12:37:03 +0200

    new method

--- novanacci/src/circuit.rs
+++ novanacci/src/circuit.rs
@@ -9,3 +9,24 @@ pub struct FibonacciObject<F: PrimeField> {
     pub x_next: F,
     pub y_next: F,
 }
+
+impl<F: PrimeField> FibonacciObject<F> {
+    pub fn new(num_iters: usize, x_0: &F, y_0: &F) -> (Vec<F>, Vec<Self>) {
+        let res: Vec<_> = (0..num_iters)
+            .scan((*x_0, *y_0), |state, _| {
+                let (x, y) = *state;
+                let x_next = y;
+                let y_next = x + y;
+                *state = (x_next, y_next);
+                Some(Self {
+                    x,
+                    y,
+                    x_next,
+                    y_next,
+                })
+            })
+            .collect();
+
+        (vec![*x_0, *y_0], res)
+    }
+}
