ed2b8eb79ec5f55c3b6c210aab13f1333c982f8e
tyshkor <tyshko1@gmail.com>
2023-05-12 12:37:40 +0200

    fibonacci circuit

--- novanacci/src/circuit.rs
+++ novanacci/src/circuit.rs
@@ -30,3 +30,59 @@ impl<F: PrimeField> FibonacciObject<F> {
         (vec![*x_0, *y_0], res)
     }
 }
+
+#[derive(Clone, Debug)]
+pub struct FibonacciCircuit<F: PrimeField> {
+    pub values: Vec<FibonacciObject<F>>,
+}
+
+impl<F: PrimeField> StepCircuit<F> for FibonacciCircuit<F> {
+    fn arity(&self) -> usize {
+        2
+    }
+
+    fn synthesize<CS: ConstraintSystem<F>>(
+        &self,
+        cs: &mut CS,
+        z: &[AllocatedNum<F>],
+    ) -> Result<Vec<AllocatedNum<F>>, SynthesisError> {
+        let mut z_out: Result<Vec<AllocatedNum<F>>, SynthesisError> =
+            Err(SynthesisError::AssignmentMissing);
+
+        let x_0 = z[0].clone();
+        let y_0 = z[1].clone();
+
+        let mut x = x_0;
+        let mut y = y_0;
+        for i in 0..self.values.len() {
+            let y_next = AllocatedNum::alloc(cs.namespace(|| format!("y_next_{i}")), || {
+                Ok(self.values[i].y_next)
+            })?;
+            let dummy = AllocatedNum::alloc(cs.namespace(|| format!("one_{i}")), || Ok(F::one()))?;
+
+            cs.enforce(
+                || format!("y_next_{i} * 1 = x_{i} + y_{i}"),
+                |lc| lc + y_next.get_variable(),
+                |lc| lc + dummy.get_variable(),
+                |lc| lc + x.get_variable() + y.get_variable(),
+            );
+            if i == self.values.len() - 1 {
+                z_out = Ok(vec![y.clone(), y_next.clone()]);
+            }
+
+            x = y;
+            y = y_next;
+        }
+        z_out
+    }
+
+    fn output(&self, z: &[F]) -> Vec<F> {
+        debug_assert_eq!(z[0], self.values[0].x);
+        debug_assert_eq!(z[1], self.values[0].y);
+
+        vec![
+            self.values[self.values.len() - 1].x_next,
+            self.values[self.values.len() - 1].y_next,
+        ]
+    }
+}
