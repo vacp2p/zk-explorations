9e688ccb827e9b5865235ccc8c5229ca67dd4e61
tyshkor <tyshko1@gmail.com>
2023-05-18 15:05:17 +0200

    public params

---
+++ nova-vdf/src/public_params.rs
@@ -0,0 +1,31 @@
+use nova::traits::{
+        circuit::TrivialTestCircuit,
+        Group,
+    };
+
+use pasta_curves::{pallas, vesta};
+
+use crate::InverseMinRootCircuit;
+
+pub type G1 = pallas::Point;
+pub type G2 = vesta::Point;
+
+pub type S1 = pallas::Scalar;
+pub type S2 = vesta::Scalar;
+
+pub type C1 = InverseMinRootCircuit<G1>;
+pub type C2 = TrivialTestCircuit<<G2 as Group>::Scalar>;
+
+pub type NovaVDFPublicParams = nova::PublicParams<
+    G1,
+    G2,
+    InverseMinRootCircuit<G1>,
+    TrivialTestCircuit<<G2 as Group>::Scalar>,
+>;
+
+pub fn public_params(num_iters_per_step: u64) -> NovaVDFPublicParams {
+    let (circuit_primary, circuit_secondary) =
+        InverseMinRootCircuit::<G1>::circuits(num_iters_per_step);
+
+    NovaVDFPublicParams::setup(circuit_primary, circuit_secondary.clone())
+}
