7b32cf66f143c71303a8bc65038d85d29b1707f4
jonesmarvin8 <83104039+jonesmarvin8@users.noreply.github.com>
2023-09-12 10:58:46 -0400

    Changing to goldilocks and kzg

--- halo2-bench/src/recursion.rs
+++ halo2-bench/src/recursion.rs
@@ -49,13 +49,13 @@ impl<const WIDTH: usize, const RATE: usize> Spec<Fp, WIDTH, RATE> for MySpec<WID
     }
 }
 
-//const K: u32 = 7;
+const K: u32 = 7;
 
-pub fn recursion(d: usize) -> bool {
+pub fn recursion(d: usize) {
     //criterion_group!(benches, criterion_benchmark);
     //criterion_main!(benches);
     //TODO: make recursion and bench_poseidon a single function. Currently two due to MySpec issues.
-    bench_poseidon::<MySpec<3,2>, 3, 2, 4>(d)
+    bench_poseidon::<MySpec<3,2>, 3, 2, 4>(d);
 }
 
 
@@ -63,34 +63,36 @@ fn bench_poseidon<S, const WIDTH: usize, const RATE: usize, const L : usize>(
     //name: &str,
     //c: &mut Criterion,
     d : usize,
-) -> bool where
+) //-> bool 
+where
     S: Spec<Fp, WIDTH, RATE> + Copy + Clone,
 {
    // let mut c:Criterion = Criterion::default();
     // Initialize the polynomial commitment parameters
-    let params: Params<vesta::Affine> = Params::new(7); //Why 7?
+    let params: Params<vesta::Affine> = Params::new(K); //Why 7?
 
     let empty_circuit = mycircuit::HashCircuit::<S, WIDTH, RATE, L> {
         message: Value::unknown(),
         _spec: PhantomData,
     };
 
+    //TODO: ISSUE HERE!
     // Initialize the proving key
     let vk = keygen_vk(&params, &empty_circuit).expect("keygen_vk should not fail");
-    let pk = keygen_pk(&params, vk, &empty_circuit).expect("keygen_pk should not fail");
+   // let pk = keygen_pk(&params, vk, &empty_circuit).expect("keygen_pk should not fail");
 
     let prover_name = "prover";
     let verifier_name = "verifier";
 
     let mut rng = OsRng;
 
-    let mut messages : Vec<[Fp;L]> = Vec::new();
+    //let mut messages : Vec<[Fp;L]> = Vec::new();
 
-    messages.push( (0..L)
+    let mut message : [Fp;L] =  (0..L)
         .map(|_| pallas::Base::random(rng))
         .collect::<Vec<_>>()
         .try_into()
-        .unwrap());
+        .unwrap();
 
     //Fix this.
     /*
@@ -100,15 +102,18 @@ fn bench_poseidon<S, const WIDTH: usize, const RATE: usize, const L : usize>(
         );
     }*/
 
-    let output = poseidon::Hash::<_, S, ConstantLength<L>, WIDTH, RATE>::init().hash(messages[0]);
+    
+    let output = poseidon::Hash::<_, S, ConstantLength<L>, WIDTH, RATE>::init().hash(message);
 
 
     let circuit = mycircuit::HashCircuit::<S, WIDTH, RATE, L> {
-        message: Value::known(messages[0]),
+        message: Value::known(message),
         _spec: PhantomData,
     };
+    
 
     // Create a proof
+    /*
     let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
     create_proof(
         &params,
@@ -119,11 +124,12 @@ fn bench_poseidon<S, const WIDTH: usize, const RATE: usize, const L : usize>(
         &mut transcript,
     )
     .expect("proof generation should not fail");
-    let proof = transcript.finalize();
+*/
+    //let proof = transcript.finalize();
 
-    let strategy = SingleVerifier::new(&params);
-    let mut transcript = Blake2bRead::<_, _, Challenge255<_>>::init(&proof[..]);
+    //let strategy = SingleVerifier::new(&params);
+    //let mut transcript = Blake2bRead::<_, _, Challenge255<_>>::init(&proof[..]);
     
-    verify_proof( &params, pk.get_vk(), strategy, &[&[&[output]]], &mut transcript).is_ok()
+    //verify_proof( &params, pk.get_vk(), strategy, &[&[&[output]]], &mut transcript).is_ok()
 
 }
