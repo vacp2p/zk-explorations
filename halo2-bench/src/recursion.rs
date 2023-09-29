32cf8fe9ca201e07f6aca1b1a33db91da6e91893
jonesmarvin8 <83104039+jonesmarvin8@users.noreply.github.com>
2023-09-29 08:15:30 -0400

    various fixes
    remove potential conflicts in cargo.toml

--- halo2-bench/src/recursion.rs
+++ halo2-bench/src/recursion.rs
@@ -1,21 +1,17 @@
-//use ff::Field;
 use halo2_proofs::{
-    circuit::{Layouter, SimpleFloorPlanner, Value},
-    plonk::{
-        create_proof, keygen_pk, keygen_vk, verify_proof, Advice, Circuit, Column,
-        ConstraintSystem, Error, Instance,
-    },
+    circuit::Value,
+    plonk::{create_proof, keygen_pk, keygen_vk, verify_proof},
     poly::{
         commitment::ParamsProver,
         kzg::{
             commitment::{KZGCommitmentScheme, ParamsKZG},
             multiopen::{ProverGWC, VerifierGWC},
-            strategy::{AccumulatorStrategy, SingleStrategy},
+            strategy::AccumulatorStrategy,
         },
         VerificationStrategy,
     },
     transcript::{
-        Blake2bRead, Blake2bWrite, Challenge255, TranscriptReadBuffer, TranscriptWrite,
+        Blake2bRead, Blake2bWrite, Challenge255, TranscriptReadBuffer,
         TranscriptWriterBuffer,
     },
 };
@@ -24,12 +20,16 @@ use halo2_proofs::halo2curves::{
     bn256::{Bn256, Fr, G1Affine},
     ff::Field,
 };
-
 use halo2_gadgets::poseidon::{
-    primitives::{self as poseidon, generate_constants, ConstantLength, Mds, Spec},
-    Hash, Pow5Chip, Pow5Config,
+    primitives::{self as poseidon, ConstantLength, Spec},
 };
-use snark_verifier::system::halo2::transcript::evm::EvmTranscript;
+
+use snark_verifier::{
+    halo2::{aggregation::{self, AggregationCircuit}, gen_snark_shplonk},
+    GWC, SHPLONK,
+};
+
+//use snark_verifier::system::halo2::transcript::evm::EvmTranscript;
 use std::convert::TryInto;
 use std::marker::PhantomData;
 
@@ -61,7 +61,6 @@ where
 
     let mut messages: Vec<[Fr; L]> = Vec::new();
     let mut circuits: Vec<HashCircuit<S, WIDTH, RATE, L>> = Vec::new();
-    let proofs: Vec<Vec<u8>> = Vec::new();
 
     messages.push(
         (0..L)
@@ -71,17 +70,21 @@ where
             .unwrap(),
     );
 
-    let output = poseidon::Hash::<_, S, ConstantLength<L>, WIDTH, RATE>::init().hash(messages[0]);
-
     circuits.push(HashCircuit::<S, WIDTH, RATE, L> {
         message: Value::known(messages[0]),
         _spec: PhantomData,
     });
 
+    //generate proofs for everything
 
-    /*
+    let mut transcripts = Vec::new();
+    let mut proofs = Vec::new();
+    //let mut snarks = Vec::new();
+
+    //Constructs each circuit
     for i in 0..d {
-        output = poseidon::Hash::<_, S, ConstantLength<L>, WIDTH, RATE>::init().hash(messages[i]);
+        let output =
+            poseidon::Hash::<_, S, ConstantLength<L>, WIDTH, RATE>::init().hash(messages[i]);
 
         messages.push(
             (0..L)
@@ -95,47 +98,56 @@ where
             message: Value::known(messages[i]),
             _spec: PhantomData,
         });
-    }
 
-    
-    for j in [0..d] {
+        transcripts.push({
+            let mut transcript = Blake2bWrite::<_, G1Affine, Challenge255<_>>::init(vec![]);
+            create_proof::<KZGCommitmentScheme<Bn256>, ProverGWC<_>, _, _, _, _>(
+                &params,
+                &pk,
+                &[circuits[i]],
+                &[&[&[messages[i + 1][0]]]],
+                rng,
+                &mut transcript,
+            )
+            .unwrap();
+            transcript.finalize()
+        });
+
         proofs.push({
             let mut transcript = Blake2bWrite::<_, G1Affine, Challenge255<_>>::init(vec![]);
             create_proof::<KZGCommitmentScheme<Bn256>, ProverGWC<_>, _, _, _, _>(
                 &params,
                 &pk,
                 &[circuits[i]],
-                &[&[&[messages[i][0]]]],
+                &[&[&[messages[i + 1][0]]]],
                 rng,
-                &mut transcripts[i],
+                &mut transcript,
             )
             .unwrap();
             transcript.finalize()
         });
-    }*/
 
-    let proof: Vec<u8> = {
-        let mut transcript = Blake2bWrite::<_, G1Affine, Challenge255<_>>::init(vec![]);
-        create_proof::<KZGCommitmentScheme<Bn256>, ProverGWC<_>, _, _, _, _>(
-            &params,
-            &pk,
-            &[circuits[0]],
-            &[&[&[output]]],
-            rng,
-            &mut transcript,
-        )
-        .unwrap();
-        transcript.finalize()
-    };
+        //TODO: Right here we need hashcircuit to be a standardplonk
+        //snarks.push(gen_snark_shplonk(&params, &pk, circuits[i], None::<&str>));
+    }
 
+    //Aggregate
+    //TODO: new pk???
+    //let agg_circuit: AggregationCircuit<GWC> = aggregation::AggregationCircuit::new(&params, snarks);
+    //let num_instances = agg_circuit.num_instances();
+    
+    //evm verify is probably overkill
+
+
+    //Change to verify agg_circuit
     let accept = {
-        let mut transcript = Blake2bRead::<_, G1Affine, _>::init(proof.as_slice());
+        let mut transcript = Blake2bRead::<_, G1Affine, _>::init(proofs[0].as_slice());
         VerificationStrategy::<_, VerifierGWC<_>>::finalize(
             verify_proof::<_, VerifierGWC<_>, _, _, _>(
                 &params.verifier_params(),
                 &pk.get_vk(),
                 AccumulatorStrategy::new(params.verifier_params()),
-                &[&[&[output]]],
+                &[&[&[messages[1][0]]]],
                 &mut transcript,
             )
             .unwrap(),
