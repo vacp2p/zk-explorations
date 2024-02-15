910cbc0ce8c364f7449005f9be5f03a6fa3d6e10
Rostyslav Tyshko <tyshko.rostyslav@gmail.com>
2024-02-15 23:12:19 +0100

    benchmark file

---
+++ halo2-gwc/benches/poseidon.rs
@@ -0,0 +1,57 @@
+use core::time::Duration;
+use criterion::*;
+
+use halo2_proofs::plonk::Circuit;
+use snark_verifier_sdk::halo2::{gen_srs};
+use snark_verifier_sdk::{
+    gen_pk,
+    halo2::{aggregation::AggregationCircuit},
+};
+use snark_verifier_sdk::{CircuitExt, GWC};
+
+criterion_group! {
+    name = recursive_snark;
+    config = Criterion::default().warm_up_time(Duration::from_millis(3000));
+    targets = bench_recursive_snark
+}
+    
+criterion_main!(recursive_snark);
+
+fn bench_recursive_snark(c: &mut Criterion) {
+  let params_app = gen_srs(8);
+  let params = gen_srs(23);
+
+  let cases = vec![3, 10, 100];
+
+  for k in cases {
+
+    let mut group = c.benchmark_group(format!(
+      "Halo2-GWC-Poseidon-num-steps-{}",
+      k
+    ));
+    group.sample_size(10);
+
+    group.bench_function("Prove", |b| {
+      b.iter(|| {
+        let snarks: Vec<_> = vec![0; k].into_iter().map(|_| halo2_gwc::gen_application_snark(&params_app)).collect();
+
+        let agg_circuit = AggregationCircuit::<GWC>::new(&params, snarks);
+        let pk = gen_pk(
+            &params,
+            &agg_circuit.without_witnesses(),
+            None,
+        );
+
+        snark_verifier_sdk::halo2::gen_proof_gwc(
+          &params,
+          &pk,
+          agg_circuit.clone(),
+          agg_circuit.instances(),
+          None,
+      );
+    
+      })
+    });
+    group.finish();
+  }
+}
