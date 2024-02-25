cd4e4e9ad2c56e3e412d85ad10b0af2cdd62cf9c
Rostyslav Tyshko <tyshko.rostyslav@gmail.com>
2024-02-25 23:14:59 +0100

    dedicate specific bench for verify

--- nova-bellman/benches/poseidon.rs
+++ nova-bellman/benches/poseidon.rs
@@ -8,7 +8,7 @@ use criterion::*;
 criterion_group! {
 name = recursive_snark;
 config = Criterion::default().warm_up_time(Duration::from_millis(3000));
-targets = bench_recursive_snark_proof
+targets = bench_recursive_snark_proof, bench_recursive_snark_verify
 }
 
 criterion_main!(recursive_snark);
@@ -51,4 +51,55 @@ fn bench_recursive_snark_proof(c: &mut Criterion) {
     });
     group.finish();
   }
+}
+
+fn bench_recursive_snark_verify(c: &mut Criterion) {
+  let cases = vec![3, 10, 100];
+
+  let mut rng = XorShiftRng::from_seed(TEST_SEED);
+
+  let x0 = Field::random(&mut rng);
+  let x1 = Field::random(&mut rng);
+  let x2 = Field::random(&mut rng);
+  let x3 = Field::random(&mut rng);
+
+  
+
+  // produce public parameters
+  let pp = public_params();
+
+
+
+for num_steps in cases {
+
+  let mut group = c.benchmark_group(format!(
+    "Nova-Poseidon-num-steps-{}",
+    num_steps
+  ));
+  group.sample_size(10);
+
+  let initial_state = vec![x0, x1, x2, x3];
+
+  let (z0, circuits) =
+  PoseidonHashChainCircuit::eval_and_make_circuits(num_steps, initial_state.clone());
+
+  let recursive_snark =
+      NovaChainHashProof::prove_recursively(&pp, &circuits, z0.clone()).unwrap();
+
+  let zi = calculate_chain_hash(initial_state, num_steps);
+
+  group.bench_function("Verify", |b| {
+    b.iter(|| {
+
+
+      let res = recursive_snark.verify(&pp, num_steps, z0.clone(), &zi);
+
+      if !res.is_ok() {
+          dbg!(&res);
+      }
+      assert!(res.unwrap());
+    })
+  });
+  group.finish();
+}
 }
