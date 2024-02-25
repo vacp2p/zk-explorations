4dbd9b0fce78a8efbd76eb745d9468ef802be447
Rostyslav Tyshko <tyshko.rostyslav@gmail.com>
2024-02-25 23:36:54 +0100

    dedicate specific bench for verify

--- arecibo/benches/poseidon.rs
+++ arecibo/benches/poseidon.rs
@@ -8,7 +8,7 @@ use criterion::*;
 criterion_group! {
 name = recursive_snark;
 config = Criterion::default().warm_up_time(Duration::from_millis(3000));
-targets = bench_recursive_snark_prove
+targets = bench_recursive_snark_prove, bench_recursive_snark_verify
 }
 
 criterion_main!(recursive_snark);
@@ -45,19 +45,60 @@ fn bench_recursive_snark_prove(c: &mut Criterion) {
         let (z0, circuits) =
         PoseidonHashChainCircuit::eval_and_make_circuits(num_steps, initial_state.clone());
 
-        let recursive_snark =
-            NovaChainHashProof::prove_recursively(&pp, &circuits, z0.clone()).unwrap();
-
-        let zi = calculate_chain_hash(initial_state, num_steps);
-
-        let res = recursive_snark.verify(&pp, num_steps, z0.clone(), &zi);
-
-        if !res.is_ok() {
-            dbg!(&res);
-        }
-        assert!(res.unwrap());
+        NovaChainHashProof::prove_recursively(&pp, &circuits, z0.clone()).unwrap();
       })
     });
     group.finish();
   }
+}
+
+fn bench_recursive_snark_verify(c: &mut Criterion) {
+  let cases = vec![3, 10, 100];
+
+  let mut rng = XorShiftRng::from_seed(TEST_SEED);
+
+  let x0 = Field::random(&mut rng);
+  let x1 = Field::random(&mut rng);
+  let x2 = Field::random(&mut rng);
+  let x3 = Field::random(&mut rng);
+
+  
+
+  // produce public parameters
+  let pp = public_params();
+
+
+
+for num_steps in cases {
+
+  let mut group = c.benchmark_group(format!(
+    "Arecibo-Poseidon-num-steps-{}",
+    num_steps
+  ));
+  group.sample_size(10);
+
+  let initial_state = vec![x0, x1, x2, x3];
+
+  let (z0, circuits) =
+  PoseidonHashChainCircuit::eval_and_make_circuits(num_steps, initial_state.clone());
+
+  let recursive_snark =
+      NovaChainHashProof::prove_recursively(&pp, &circuits, z0.clone()).unwrap();
+
+  let zi = calculate_chain_hash(initial_state, num_steps);
+
+  group.bench_function("Verify", |b| {
+    b.iter(|| {
+
+
+      let res = recursive_snark.verify(&pp, num_steps, z0.clone(), &zi);
+
+      if !res.is_ok() {
+          dbg!(&res);
+      }
+      assert!(res.unwrap());
+    })
+  });
+  group.finish();
+}
 }
