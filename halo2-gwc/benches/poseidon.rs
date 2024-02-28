98a4c7abc04cccbe90df3772ac40698044c0f426
Rostyslav Tyshko <tyshko.rostyslav@gmail.com>
2024-02-28 05:38:41 +0100

    make dedicated proove and verify benchmarks

--- halo2-gwc/benches/poseidon.rs
+++ halo2-gwc/benches/poseidon.rs
@@ -22,12 +22,12 @@ use snark_verifier_sdk::{CircuitExt, NativeLoader, GWC};
 criterion_group! {
     name = recursive_snark;
     config = Criterion::default().warm_up_time(Duration::from_millis(3000));
-    targets = bench_recursive_snark
+    targets = bench_recursive_snark_proove,bench_recursive_snark_verify
 }
     
 criterion_main!(recursive_snark);
 
-fn bench_recursive_snark(c: &mut Criterion) {
+fn bench_recursive_snark_proove(c: &mut Criterion) {
   let params_app = gen_srs(8);
   let params = gen_srs(23);
 
@@ -68,6 +68,75 @@ fn bench_recursive_snark(c: &mut Criterion) {
       create_proof::<_, ProverGWC<_>, _, _, _, _>(&params, &pk, &[circuit.clone()], &[&instances], rng.clone(), &mut transcript)
           .unwrap();
       let proof = transcript.finalize();
+  
+
+  
+      // validate proof before caching
+      assert!({
+          let mut transcript_read =
+              PoseidonTranscript::<NativeLoader, &[u8]>::from_spec(&proof[..], POSEIDON_SPEC.clone());
+          VerificationStrategy::<_, VerifierGWC<_>>::finalize(
+              verify_proof::<_, VerifierGWC<_>, _, _, _>(
+                  params.verifier_params(),
+                  pk.get_vk(),
+                  AccumulatorStrategy::new(params.verifier_params()),
+                  &[instances.as_slice()],
+                  &mut transcript_read,
+              )
+              .unwrap(),
+          )
+      });
+    
+      })
+    });
+    group.finish();
+  }
+}
+
+fn bench_recursive_snark_verify(c: &mut Criterion) {
+  let params_app = gen_srs(8);
+  let params = gen_srs(23);
+
+  let cases = vec![3, 10, 100];
+
+  for k in cases {
+
+    let mut group = c.benchmark_group(format!(
+      "Halo2-GWC-Poseidon-num-steps-{}",
+      k
+    ));
+    group.sample_size(10);
+
+    let snarks: Vec<_> = vec![0; k].into_iter().map(|_| halo2_gwc::gen_application_snark(&params_app)).collect();
+
+    let agg_circuit = AggregationCircuit::<GWC>::new(&params, snarks);
+    let pk = gen_pk(
+        &params,
+        &agg_circuit.without_witnesses(),
+        None,
+    );
+
+    let circuit = agg_circuit.clone();
+    let instances = agg_circuit.instances();
+
+    let rng = StdRng::from_entropy();
+    let instances = instances.iter().map(Vec::as_slice).collect_vec();
+
+
+    let mut transcript =
+    PoseidonTranscript::<NativeLoader, _>::from_spec(vec![], POSEIDON_SPEC.clone());
+
+create_proof::<_, ProverGWC<_>, _, _, _, _>(&params, &pk, &[circuit.clone()], &[&instances], rng.clone(), &mut transcript)
+    .unwrap();
+let proof = transcript.finalize();
+
+    group.bench_function("Prove", |b| {
+      b.iter(|| {
+  
+      
+  
+
+
   
 
   
