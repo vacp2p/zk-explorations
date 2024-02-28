e3969a9848f1fb6f9a392ba986f68375faa134ae
Rostyslav Tyshko <tyshko.rostyslav@gmail.com>
2024-02-28 05:14:59 +0100

    one more astraction level less

--- halo2-shplonk/benches/poseidon.rs
+++ halo2-shplonk/benches/poseidon.rs
@@ -1,18 +1,25 @@
 use core::time::Duration;
+use std::fs;
 use std::path::{Path, PathBuf};
 use criterion::*;
 
 use halo2_gadgets::sinsemilla::primitives::C;
-use halo2_proofs::plonk::Circuit;
+use halo2_proofs::plonk::{create_proof, verify_proof, Circuit, Instance};
+use halo2_proofs::poly::commitment::ParamsProver;
 use halo2_proofs::poly::kzg::multiopen::{ProverSHPLONK, VerifierSHPLONK};
+use halo2_proofs::poly::kzg::strategy::AccumulatorStrategy;
+use halo2_proofs::poly::VerificationStrategy;
 use halo2curves::bn256::Bn256;
+use itertools::Itertools;
+use rand::rngs::StdRng;
+use rand::SeedableRng;
 use snark_verifier::pcs::kzg::{Bdfg21, KzgAs};
-use snark_verifier_sdk::halo2::{gen_proof, gen_srs};
+use snark_verifier_sdk::halo2::{gen_proof, gen_srs, PoseidonTranscript, POSEIDON_SPEC};
 use snark_verifier_sdk::{
     gen_pk,
     halo2::{aggregation::AggregationCircuit},
 };
-use snark_verifier_sdk::{CircuitExt, SHPLONK};
+use snark_verifier_sdk::{read_instances, write_instances, CircuitExt, NativeLoader, SHPLONK};
 
 criterion_group! {
     name = recursive_snark;
@@ -45,13 +52,41 @@ fn bench_recursive_snark(c: &mut Criterion) {
             None,
         );
 
+    let circuit = agg_circuit.clone();
+    let instances = agg_circuit.instances();
+
     group.bench_function("Prove", |b| {
       b.iter(|| {
         
-
-
-        gen_proof::<AggregationCircuit<KzgAs<Bn256, Bdfg21>>, ProverSHPLONK<_>, VerifierSHPLONK<_>>(&params, &pk, agg_circuit.clone(), agg_circuit.instances(), None::<(PathBuf, PathBuf)>);
-    
+  
+      let instances = instances.iter().map(Vec::as_slice).collect_vec();
+  
+  
+  
+      let mut transcript =
+          PoseidonTranscript::<NativeLoader, _>::from_spec(vec![], POSEIDON_SPEC.clone());
+      let rng = StdRng::from_entropy();
+      create_proof::<_, ProverSHPLONK<_>, _, _, _, _>(&params, &pk, &[circuit.clone()], &[&instances], rng, &mut transcript)
+          .unwrap();
+      let proof = transcript.finalize();
+  
+  
+      // validate proof before caching
+      assert!({
+          let mut transcript_read =
+              PoseidonTranscript::<NativeLoader, &[u8]>::from_spec(&proof[..], POSEIDON_SPEC.clone());
+          VerificationStrategy::<_, VerifierSHPLONK<_>>::finalize(
+              verify_proof::<_, VerifierSHPLONK<_>, _, _, _>(
+                  params.verifier_params(),
+                  pk.get_vk(),
+                  AccumulatorStrategy::new(params.verifier_params()),
+                  &[instances.as_slice()],
+                  &mut transcript_read,
+              )
+              .unwrap(),
+          )
+      });
+  
       })
     });
     group.finish();
