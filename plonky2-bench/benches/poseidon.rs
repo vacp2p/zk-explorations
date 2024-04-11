c668a02b822a5f3d89870113cb8f838100c69b72
Rostyslav Tyshko <tyshko.rostyslav@gmail.com>
2024-04-11 13:55:54 +0200

    show proof size

--- plonky2-bench/benches/poseidon.rs
+++ plonky2-bench/benches/poseidon.rs
@@ -132,6 +132,11 @@ fn bench_recursive_snark_verify(c: &mut Criterion) {
             proof = cyclic_circuit_data.prove(pw).unwrap();
         }
 
+        println!(
+          "Plonky2 SNARK::len {:?} bytes",
+          proof.to_bytes().len()
+        );
+
         check_cyclic_proof_verifier_data(
             &proof,
             &cyclic_circuit_data.verifier_only,
